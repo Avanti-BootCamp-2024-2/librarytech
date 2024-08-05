1bcf3fef97b0a2c6d70ef2dabfe8490f92a0e36b
Adriano Silva <lucio.adriano@gmail.com>
2024-08-05 16:38:55 -0300

    corrige erro no cadastro de usuario

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const rotas = require('./rotas');
 const app = express()
 const port = 3000
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use(rotas);
